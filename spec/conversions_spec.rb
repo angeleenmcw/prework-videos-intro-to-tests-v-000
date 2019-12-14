fa9588cd74d2a5edc8bba554cad522223687923a
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2019-12-14 09:21:17 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -3,8 +3,8 @@ require_relative '../conversions.rb'
 describe "conversions" do
   describe '#ounces_to_grams' do
     it 'given zero, returns 0.0' do
-      grams = ounces_to_grams(28.345)
-      expect(grams).to eq(2.0)
+      grams = ounces_to_grams(28.3495)
+      expect(grams).to eq(28.3495)
     end
   end
 end
